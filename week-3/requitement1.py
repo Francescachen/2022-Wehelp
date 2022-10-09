46b6a45e9008e179a064b5a7eb93fb85a920bd3f
Francesca <80885004+Francescachen@users.noreply.github.com>
2022-10-09 11:55:27 +0800

    Add files via upload

---
+++ week-3/requitement1.py
@@ -0,0 +1,23 @@
+import json
+import urllib.request as request
+import csv
+
+src = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
+with request.urlopen(src) as response:
+    dataset = json.load(response)
+    dataset = dataset["result"]["results"]
+
+with open("data.csv", mode="w") as file:
+    # get csv writer
+    writer = csv.writer(file)
+    
+    # access every data, and save data's attribute.
+    # Finally, use writerrow() to write data.
+    for data in dataset:
+        if int(data["xpostDate"][:4]) >= 2015:
+            stitle = data["stitle"]
+            address = data["address"][5:8]
+            longitude = data["longitude"]
+            latitude = data["latitude"]
+            imgURL = data["file"].lower().split(".jpg")[0] + ".jpg"  # for get first url.
+            writer.writerow([stitle, address, longitude, latitude, imgURL])
